c26815e526230fc5e32ed3be6b44842ad37c5e57
nazavr322 <91838295+nazavr322@users.noreply.github.com>
2022-03-01 19:56:23 +0200

    Added possibility to read channels from file

--- report.py
+++ report.py
@@ -1,32 +1,70 @@
 import sys
 import asyncio
+import time
+import random
 from telethon import TelegramClient, events, functions, types
+from argparse import ArgumentParser, FileType
 
-peers = sys.argv[4:]
+def create_parser():
+    """Initializes parser with all parameters"""
 
-api_id = sys.argv[1]
-api_hash = sys.argv[2]
-message = sys.argv[3]
+    parser = ArgumentParser(prog="report_telegram_channels")
+    parser.add_argument('api_id', help='Your API id')
+    parser.add_argument('api_hash', help='Your API hash')
+    parser.add_argument('message', help='Report message')
+    
+    # this controls that one of this arguments (-c or -f)
+    # must be specified, but not both at a time
+    mutual_group = parser.add_mutually_exclusive_group(required=True)
+    mutual_group.add_argument('-c', '--channels', nargs='*', dest='channels', 
+                              help='Channels to report')
+    mutual_group.add_argument('-f', '--file', dest='filepath', type=FileType('r'),
+                              help='File with channels to report')
+    return parser
 
 async def main():
-  client = TelegramClient('session_name', api_id, api_hash)
+    parser = create_parser()
+    args, _ = parser.parse_known_args()
+    # initialize variables
+    api_id = args.api_id
+    api_hash = args.api_hash
+    message = args.message
+    
+    client = TelegramClient('session_name', api_id, api_hash)
+    await client.start()
 
-  await client.start()
 
-  for p in peers:
-    result = None
+    if args.filepath is not None:
+        # channels are taken from file
+        peers = args.filepath.read().splitlines()
+    else:
+        # channels are taken from command line
+        peers = args.channels
+    for idx, p in enumerate(peers):
+        result = None
 
-    try:
-      result = await client(functions.account.ReportPeerRequest(
-        peer=p,
-        reason=types.InputReportReasonOther(),
-        message=message
-      ))
-    except:
-      print('no user', p)
-      continue
+        try:
+            result = await client(functions.account.ReportPeerRequest(
+                peer=p,
+                reason=types.InputReportReasonOther(),
+                message=message
+                ))
 
-    print(p, result)
+            print(p, result)
+
+            if idx > 0 and not idx % 10:
+                sleep_for = random.randint(50, 60)
+
+                print('sleep for {}s before the next batch of 10'.format(sleep_for))
+                time.sleep(sleep_for)
+            else:
+                sleep_for = random.randint(10, 20)
+
+                print('sleep for {}s...'.format(sleep_for))
+                time.sleep(sleep_for)
+        except:
+            print('no user', p)
+            continue
 
 if __name__ == "__main__":
-  asyncio.run(main())
+    asyncio.run(main())
